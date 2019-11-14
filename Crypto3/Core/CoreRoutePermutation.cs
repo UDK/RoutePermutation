4f9e01a21b11b7d0dd23f363af2af5c5e0888403
szfsfrsefgdg iskdmwhsd <udk1@bk.ru>
2019-11-14 20:36:50 +0300

    init

--- Crypto3/Core/CoreRoutePermutation.cs
+++ Crypto3/Core/CoreRoutePermutation.cs
@@ -105,7 +105,7 @@ namespace Crypto3.Core
             return retStr(matrix);
         }
         public void PrintArray(char[,] array)
-        {
+        { 
             for (int i = 0; i < array.GetLength(0); i++)
             {
                 for (int j = 0; j < array.GetLength(1); j++)
